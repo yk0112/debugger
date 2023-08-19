13668713270e3e23ae280a7373a8586e3f717f05
yk0112 <yosikou0726@gmail.com>
2023-08-19 20:01:12 +0900

    feat: ブレークポイントがユーザ指定のものかシステム側で設定したものかを区別するパラメータを追加

--- include/breakpoint.hpp
+++ include/breakpoint.hpp
@@ -4,16 +4,17 @@
 #include <cstdint>
 
 class breakpoint {
- public:
-  breakpoint(pid_t pid, std::intptr_t addr)
-      : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_saved_data{} {}
+public:
+  breakpoint(pid_t pid, std::intptr_t addr, bool is_users)
+      : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_saved_data{},
+        is_users_breakpoint{is_users} {}
 
   void enable() {
     auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
-    m_saved_data = static_cast<uint8_t>(data & 0xff);  // save bottom byte
+    m_saved_data = static_cast<uint8_t>(data & 0xff); // save bottom byte
     uint64_t int3 = 0xcc;
     uint64_t data_with_int3 =
-        ((data & ~0xff) | int3);  // set bottom byte to 0xcc
+        ((data & ~0xff) | int3); // set bottom byte to 0xcc
     ptrace(PTRACE_POKEDATA, m_pid, m_addr, data_with_int3);
 
     m_enabled = true;
@@ -30,7 +31,9 @@ class breakpoint {
   auto is_enable() const -> bool { return m_enabled; }
   auto get_address() const -> std::intptr_t { return m_addr; }
 
- private:
+  bool is_users_breakpoint;
+
+private:
   pid_t m_pid;
   std::intptr_t m_addr;
   bool m_enabled;
