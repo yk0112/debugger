8dd9a20d17f8a625e93545ae299e7e71c464bee9
yk0112 <yosikou0726@gmail.com>
2023-08-24 02:41:33 +0900

    docs: 一部コメントの削除

--- include/register.hpp
+++ include/register.hpp
@@ -72,73 +72,14 @@ const std::array<reg_descriptor, n_registers> g_register_descriptors{{
     {reg::gs, 55, "gs"},
 }};
 
-// unsigned long long int* select_register(user_regs_struct regs, reg r) {
-//   switch (r) {
-//     case reg::r15:
-//       return &regs.r15;
-//     case reg::r14:
-//       return &regs.r14;
-//     case reg::r13:
-//       return &regs.r13;
-//     case reg::r12:
-//       return &regs.r12;
-//     case reg::rbp:
-//       return &regs.rbp;
-//     case reg::rbx:
-//       return &regs.rbx;
-//     case reg::r11:
-//       return &regs.r11;
-//     case reg::r10:
-//       return &regs.r10;
-//     case reg::r9:
-//       return &regs.r9;
-//     case reg::r8:
-//       return &regs.r8;
-//     case reg::rax:
-//       return &regs.rax;
-//     case reg::rcx:
-//       return &regs.rcx;
-//     case reg::rdx:
-//       return &regs.rdx;
-//     case reg::rsi:
-//       return &regs.rsi;
-//     case reg::rdi:
-//       return &regs.rdi;
-//     case reg::orig_rax:
-//       return &regs.orig_rax;
-//     case reg::rip:
-//       return &regs.rip;
-//     case reg::cs:
-//       return &regs.cs;
-//     case reg::eflags:
-//       return &regs.eflags;
-//     case reg::rsp:
-//       return &regs.rsp;
-//     case reg::ss:
-//       return &regs.ss;
-//     case reg::fs_base:
-//       return &regs.fs_base;
-//     case reg::gs_base:
-//       return &regs.gs_base;
-//     case reg::ds:
-//       return &regs.ds;
-//     case reg::es:
-//       return &regs.es;
-//     case reg::fs:
-//       return &regs.fs;
-//     case reg::gs:
-//       return &regs.gs;
-//   }
-// }
-
 uint64_t get_register_value(pid_t pid, reg r) {
   user_regs_struct regs;
   ptrace(PTRACE_GETREGS, pid, nullptr, &regs);
   auto it =
       std::find_if(begin(g_register_descriptors), end(g_register_descriptors),
-                   [r](auto&& rd) { return rd.r == r; });
+                   [r](auto &&rd) { return rd.r == r; });
 
-  return *(reinterpret_cast<uint64_t*>(&regs) +
+  return *(reinterpret_cast<uint64_t *>(&regs) +
            (it - begin(g_register_descriptors)));
 }
 
@@ -147,17 +88,17 @@ void set_register_value(pid_t pid, reg r, uint64_t value) {
   ptrace(PTRACE_GETREGS, pid, nullptr, &regs);
   auto it =
       std::find_if(begin(g_register_descriptors), end(g_register_descriptors),
-                   [r](auto&& rd) { return rd.r == r; });
+                   [r](auto &&rd) { return rd.r == r; });
 
-  *(reinterpret_cast<uint64_t*>(&regs) + (it - begin(g_register_descriptors))) =
-      value;
+  *(reinterpret_cast<uint64_t *>(&regs) +
+    (it - begin(g_register_descriptors))) = value;
   ptrace(PTRACE_SETREGS, pid, nullptr, &regs);
 }
 
 uint64_t get_register_value_from_dwarf_register(pid_t pid, unsigned regnum) {
   auto it =
       std::find_if(g_register_descriptors.begin(), g_register_descriptors.end(),
-                   [regnum](auto&& rd) { return rd.dwarf_r == regnum; });
+                   [regnum](auto &&rd) { return rd.dwarf_r == regnum; });
   if (it == g_register_descriptors.end()) {
     throw std::out_of_range{"Unknown dwarf register"};
   }
@@ -167,13 +108,13 @@ uint64_t get_register_value_from_dwarf_register(pid_t pid, unsigned regnum) {
 std::string get_register_name(reg r) {
   auto it =
       std::find_if(begin(g_register_descriptors), end(g_register_descriptors),
-                   [r](auto&& rd) { return rd.r == r; });
+                   [r](auto &&rd) { return rd.r == r; });
   return it->name;
 }
 
-reg get_register_from_name(const std::string& name) {
+reg get_register_from_name(const std::string &name) {
   auto it =
       std::find_if(begin(g_register_descriptors), end(g_register_descriptors),
-                   [name](auto&& rd) { return rd.name == name; });
+                   [name](auto &&rd) { return rd.name == name; });
   return it->r;
 }
